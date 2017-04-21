380d83911351bfe60c336f83eed31ca636b3b45c
Mike Potjer <potjerm@gmail.com>
2017-04-20 22:14:33 -0400

    Complete Chapter 2
    Complete exercises for chapter 2.

---
+++ Chapter02/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Output test to the screen.
+			Console.WriteLine("The first app in Beginning Visual C# 2015!");
+			Console.ReadKey();
+		}
+	}
+}
