9e8425efa879f4f4ef44d78a9fbc8f1bdd12c7aa
Mike Potjer <potjerm@gmail.com>
2017-05-20 22:25:43 -0400

    Working on Chapter 6
    Working on chapter 6, but not done yet. Completed the section on the Main() function.

---
+++ Chapter06/Ch06Ex01/Ch06Ex01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Ch06Ex01
+{
+    class Program
+    {
+        // This is a global variable.
+        static string myString;
+
+        static void Write()
+        {
+            // This is a local variable.
+            string myString = "String defined in Write()";
+            WriteLine("Now in Write()");
+            WriteLine($"Local myString = {myString}");
+            WriteLine($"Global myString = {Program.myString}");
+        }
+
+        static void Main(string[] args)
+        {
+            // This is a local variable.
+            string myString = "String defined in Main()";
+            Program.myString = "Global string";
+            Write();
+            WriteLine("\nNow in Main()");
+            WriteLine($"Local myString = {myString}");
+            WriteLine($"Global myString = {Program.myString}");
+            ReadKey();
+        }
+    }
+}
---
+++ Chapter06/Ch06Ex03/Ch06Ex03/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Ch06Ex03
+{
+    class Program
+    {
+        // The params keyword indicates a parameter array.  This can be used to pass a variable
+        // number of parameters to a function (0 or more), but all of the same type.  There can
+        // be other parameters for the function, but params must always be last. and there can
+        // be only one.
+        static int SumVals(params int[] vals)
+        {
+            int sum = 0;
+            foreach (int val in vals)
+            {
+                sum += val;
+            }
+            return sum;
+        }
+
+        static void Main(string[] args)
+        {
+            int sum = SumVals(1, 5, 2, 9, 8);
+            WriteLine($"Summed Values = {sum}");
+            ReadKey();
+        }
+    }
+}
