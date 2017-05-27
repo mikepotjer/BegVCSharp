5369b64101854f8b516756b336708604545aed77
Mike Potjer <potjerm@gmail.com>
2017-05-27 15:56:05 -0400

    Complete Chapter 7
    Complete exercises for Chapter 7.

---
+++ Chapter07/Ch07Ex99x7_2/Ch07Ex99x7_2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Ch07Ex99x7_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                WriteLine($"Processing item {i}");
+                if (i == 5000)
+                    WriteLine(args[999]);
+            }
+            ReadKey();
+        }
+    }
+}
---
+++ Chapter07/Ch07Ex99x7_4/Ch07Ex99x7_4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Ch07Ex99x7_4
+{
+    class Program
+    {
+        enum Orientation : byte
+        {
+            North = 1,
+            South,
+            East,
+            West
+        }
+
+        static void Main(string[] args)
+        {
+            Orientation myDirection;
+            for (byte myByte = 0; myByte < 10; myByte++)
+            {
+                try
+                {
+                    myDirection = checked((Orientation)myByte);
+                    if (myDirection < Orientation.North || myDirection > Orientation.West)
+                    {
+                        throw new ArgumentOutOfRangeException("myByte", myByte,
+                            "Value must be between 1 and 4");
+                    }
+                }
+                catch (ArgumentOutOfRangeException e)
+                {
+                    WriteLine($"Error: {e.Message}\nAssigning default value to {Orientation.North}.");
+                    myDirection = Orientation.North;
+                }
+                WriteLine($"myDirection = {myDirection}");
+            }
+            ReadKey();
+        }
+    }
+}
