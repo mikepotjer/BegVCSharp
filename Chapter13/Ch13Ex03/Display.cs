53553e1e551829d2473d9c5eee51e84f77547b38
Mike Potjer <potjerm@gmail.com>
2017-08-01 21:49:13 -0400

    Complete Chapter 13, Exercise 3
    Complete exercise 3 from chapter 13.

---
+++ Chapter13/Ch13Ex03/Display.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Ch13Ex03
+{
+    public class Display
+    {
+        // Define a method with the signature required to be able to subscribe to the event
+        // defined in the Connection class. Since the subscriber method is in a different
+        // class, it has to be public.
+        public void DisplayMessage(object source, MessageArrivedEventArgs e)
+        {
+            // The source object needs to be cast to access its custom properties, but since
+            // we are using a specific arguments class, we can access the message directly.
+            WriteLine($"Message arrived from: {((Connection)source).Name}");
+            WriteLine($"Message text: {e.Message}");
+        }
+    }
+}
---
+++ Chapter13/Ch13Ex03/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Ch13Ex03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Connection myConnection1 = new Connection();
+            myConnection1.Name = "First connection.";
+            Connection myConnection2 = new Connection();
+            myConnection2.Name = "Second connection.";
+            Display myDisplay = new Display();
+
+            // Setup the Display object DisplayMessage() method as a subscriber to the MessageArrived
+            // event of the Connection objects.
+            myConnection1.MessageArrived += myDisplay.DisplayMessage;
+            myConnection2.MessageArrived += myDisplay.DisplayMessage;
+
+            // Calling the Connect() method will start the object polling for messages.
+            myConnection1.Connect();
+            myConnection2.Connect();
+            ReadKey();
+        }
+    }
+}
