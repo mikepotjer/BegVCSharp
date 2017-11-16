e5481d0385881b9300982d094ee2f448830b302d
Mike Potjer <potjerm@gmail.com>
2017-11-15 21:17:36 -0500

    Chapter 15, add main card game window
    Add the main game window and begin to add functionality.

---
+++ Chapter14/KarliCards/KarliCards Gui/GameClient.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace KarliCards_Gui
+{
+    /// <summary>
+    /// Interaction logic for GameClient.xaml
+    /// </summary>
+    public partial class GameClient : Window
+    {
+        public GameClient()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Method used for the CanExecute part of the command bindings defined in the window.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CommandCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (e.Command == ApplicationCommands.Close)
+                e.CanExecute = true;
+            if (e.Command == ApplicationCommands.Save)
+                e.CanExecute = false;
+            e.Handled = true;
+        }
+
+        /// <summary>
+        /// Method used for the Executed part of the command bindings defined in the window.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CommandExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (e.Command == ApplicationCommands.Close)
+                this.Close();
+            e.Handled = true;
+        }
+    }
+}
