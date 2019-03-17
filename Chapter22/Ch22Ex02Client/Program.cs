3fe6b3ccd4943b207534d2b7c6ec495b1fb2a8e8
Mike Potjer <potjerm@gmail.com>
2019-03-16 21:02:24 -0400

    Complete Chapter 22, Example 2
    Create a WCF service using service contracts.

---
+++ Chapter22/Ch22Ex02Client/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using System.ServiceModel;
+using Ch22Ex02Contracts;
+
+namespace Ch22Ex02Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] people = new Person[]
+            {
+                new Person {Mark = 46, Name = "Jim"},
+                new Person {Mark = 73, Name = "Mike"},
+                new Person {Mark = 92, Name = "Stefan"},
+                new Person {Mark = 24, Name = "Arthur"}
+            };
+
+            WriteLine("People:");
+            OutputPeople(people);
+
+            // Create a proxy class implementing the IAwardService client.
+            IAwardService client = ChannelFactory<IAwardService>.CreateChannel(
+                new WSHttpBinding(),
+                new EndpointAddress("http://localhost:2919/AwardService.svc"));
+            client.SetPassMark(70);
+
+            Person[] awardedPeople = client.GetAwardedPeople(people);
+
+            WriteLine();
+            WriteLine("Awarded People:");
+            OutputPeople(awardedPeople);
+            ReadKey();
+        }
+
+        private static void OutputPeople(Person[] people)
+        {
+            foreach (Person person in people)
+                WriteLine($"{person.Name}, mark: {person.Mark}");
+        }
+    }
+}
---
+++ Chapter22/Ch22Ex02Contracts/IAwardService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace Ch22Ex02Contracts
+{
+    // This service requires state, hence the SessionMode.Required
+    [ServiceContract(SessionMode = SessionMode.Required)]
+    public interface IAwardService
+    {
+        // This is the operation that sets the state, thus IsInitiating is true.
+        // It doesn't return anything, therefore IsOneWay is true.
+        [OperationContract(IsOneWay = true, IsInitiating = true)]
+        void SetPassMark(int passMark);
+
+        [OperationContract]
+        Person[] GetAwardedPeople(Person[] peopleToTest);
+    }
+}
